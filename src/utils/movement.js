595b406956aed8306ed85ca33809894dbbc2f4bc
Fineas Anton <fineas.anton@gmail.com>
2021-02-02 16:57:20 -0600

    movement logic makes more sense now

--- src/utils/movement.js
+++ src/utils/movement.js
@@ -1,8 +1,9 @@
-//for swipeup we are not including the first row because we do not need to swipeup any more, that is the max
+//for swipeup we start at the end of the array and make our way backwards and push items to the top
+//we do not include the first row because we do not want to exceed our boundary
 
 export function swipeUp(gameGrid) {
-  for (let i = 1; i < gameGrid.length; i++) {
-    for (let j = 0; j < gameGrid[i].length; j++) {
+  for (let i = gameGrid.length - 1; i > 0; i--) {
+    for (let j = gameGrid[i].length - 1; j >= 0; j--) {
       if (gameGrid[i][j] !== null) {
         gameGrid[i - 1][j] = gameGrid[i][j];
         gameGrid[i][j] = null;
@@ -12,11 +13,12 @@ export function swipeUp(gameGrid) {
   return gameGrid;
 }
 
-//for swipeleft we are not including the first column because we cannot exceed the boundary
+//for swipeleft we start at the end of the array making our way backwards to push all items to the left
+//we do not include the left column because we do not want to exceed the boundary when moving items
 
 export function swipeLeft(gameGrid) {
-  for (let i = 0; i < gameGrid.length; i++) {
-    for (let j = 1; j < gameGrid[i].length; j++) {
+  for (let i = gameGrid.length - 1; i >= 0; i--) {
+    for (let j = gameGrid[i].length - 1; j > 0; j--) {
       if (gameGrid[i][j] !== null) {
         gameGrid[i][j - 1] = gameGrid[i][j];
         gameGrid[i][j] = null;
@@ -27,11 +29,10 @@ export function swipeLeft(gameGrid) {
   return gameGrid;
 }
 
-//for swipedown we start at the end and make our way backwards through the array so we can move the tiles forward, we are not including the
-//last row because we cannot exceed the boundary
+//for swipedown we start at the beginning of the array and make sure to not include the bottom row to not exceed boundary
 export function swipeDown(gameGrid) {
-  for (let i = gameGrid.length - 2; i >= 0; i--) {
-    for (let j = 0; j <= gameGrid[i].length - 1; j++) {
+  for (let i = 0; i < gameGrid.length - 1; i++) {
+    for (let j = 0; j < gameGrid[i].length; j++) {
       if (gameGrid[i][j] !== null) {
         gameGrid[i + 1][j] = gameGrid[i][j];
         gameGrid[i][j] = null;
@@ -42,11 +43,11 @@ export function swipeDown(gameGrid) {
   return gameGrid;
 }
 
-//for swipedown we start at the end of the array, making our way backwards so we can move the tiles forward
-//we do not include the far right column because we cannot exceed our boundary when looping through array
+//for swipe right we start at the beginning and push everything from left to right exlcuding the right column because
+//we do not want to exceed the boundary of the array index
 
 export function swipeRight(gameGrid) {
-  for (let i = gameGrid.length - 1; i >= 0; i--) {
+  for (let i = 0; i < gameGrid.length; i++) {
     for (let j = 0; j < gameGrid[i].length - 1; j++) {
       if (gameGrid[i][j] !== null) {
         gameGrid[i][j + 1] = gameGrid[i][j];
