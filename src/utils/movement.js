bc0698dc9f47ad58a5ad6b2fbf1f0e7bbe323a75
Fineas Anton <fineas.anton@gmail.com>
2021-02-05 16:56:39 -0600

    added stricter movement

--- src/utils/movement.js
+++ src/utils/movement.js
@@ -2,11 +2,13 @@
 //we do not include the first row because we do not want to exceed our boundary
 
 export function swipeUp(gameGrid) {
-  for (let i = 1; i < gameGrid.length; i++) {
-    for (let j = 0; j < gameGrid[i].length; j++) {
+  for (let i = gameGrid.length - 1; i > 0; i--) {
+    for (let j = gameGrid[i].length - 1; j >= 0; j--) {
       if (gameGrid[i][j] !== 0) {
-        gameGrid[i - 1][j] = gameGrid[i][j];
-        gameGrid[i][j] = 0;
+        while (gameGrid[i - 1][j] === 0) {
+          gameGrid[i - 1][j] = gameGrid[i][j];
+          gameGrid[i][j] = 0;
+        }
       }
     }
   }
